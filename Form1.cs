cad5385c95a5581ad23b4c584764e7b773373971
ruben dario cordoba cuellar <ruben0793@hotmail.com>
2022-05-14 18:20:03 -0500

    Parte A consulta Api rest y insertar informacion en base de datos lista

---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using RestSharp;
+using System;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Dynamic;
+using Newtonsoft.Json.Converters;
+using SateliteMautic.Model;
+
+namespace SateliteMautic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://mautic.clinicametco.com/api/contacts");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", "Basic bWV0Y29hZG1pbjpNNHV0MWNNZXRjMCoq");
+            IRestResponse response = client.Execute(request);
+
+            dynamic config = JsonConvert.DeserializeObject<ExpandoObject>(response.Content.ToString(), new ExpandoObjectConverter());
+
+            Contacto cont = new Contacto();
+            bool ok;
+            string text = "";
+            foreach (var enabledEndpoint in config.contacts)
+            {
+                cont.Id = enabledEndpoint.Value.fields.all.id;
+                text = cont.Id;
+                if (enabledEndpoint.Value.fields.all.firstname == null) {
+                    cont.Nombre = "";
+                }
+                else
+                {
+                    cont.Nombre = enabledEndpoint.Value.fields.all.firstname;
+                }
+                if (enabledEndpoint.Value.fields.all.lastname == null)
+                {
+                    cont.Apellido = "";
+                }
+                else
+                {
+                    cont.Apellido = enabledEndpoint.Value.fields.all.lastname;
+                }
+                if (enabledEndpoint.Value.fields.all.mobile == null)
+                {
+                    cont.Celular = "0";
+                }
+                else
+                {
+                    cont.Celular = enabledEndpoint.Value.fields.all.mobile;
+                }
+                if (enabledEndpoint.Value.fields.all.email == null)
+                {
+                    cont.email = "";
+                }
+                else
+                {
+                    cont.email = enabledEndpoint.Value.fields.all.email;
+                }
+                cont.Integrado = 0;
+                cont.FechaRegistro = DateTime.Now;
+
+                ok = cont.mtd_registrar();
+
+                if (ok)
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\logsapi\Integrado\log.txt", true))
+                    {
+                        file.WriteLine("Integrado: " + text + " " + DateTime.Now.ToString());
+                    }
+                }
+                else
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\logsapi\Error\log.txt", true))
+                    {
+                        file.WriteLine("Fallo: " + text + " " + DateTime.Now.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Model/Contacto.cs
@@ -0,0 +1,81 @@
+﻿using SateliteMautic.db;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SateliteMautic.Model
+{
+    public class Contacto
+    {
+        //instancias
+        cls_conexion cls_cn = new cls_conexion();
+        cls_datos cls_datos = new cls_datos();
+        string v_query = "";
+        DataTable v_dt = new DataTable();
+        SqlParameter[] Parametros;
+        bool v_ok;
+
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Celular { get; set; }
+        public string email { get; set; }
+        public int Integrado { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        private void mtd_asignaParametros()
+        {
+            Parametros = new SqlParameter[7];
+
+            Parametros[0] = new SqlParameter();
+            Parametros[0].ParameterName = "@Id";
+            Parametros[0].SqlDbType = SqlDbType.VarChar;
+            Parametros[0].SqlValue = Id;
+
+            Parametros[1] = new SqlParameter();
+            Parametros[1].ParameterName = "@Nombre";
+            Parametros[1].SqlDbType = SqlDbType.VarChar;
+            Parametros[1].SqlValue = Nombre;
+
+            Parametros[2] = new SqlParameter();
+            Parametros[2].ParameterName = "@Apellido";
+            Parametros[2].SqlDbType = SqlDbType.VarChar;
+            Parametros[2].SqlValue = Apellido;
+
+            Parametros[3] = new SqlParameter();
+            Parametros[3].ParameterName = "@Celular";
+            Parametros[3].SqlDbType = SqlDbType.VarChar;
+            Parametros[3].SqlValue = Celular;
+
+            Parametros[4] = new SqlParameter();
+            Parametros[4].ParameterName = "@email";
+            Parametros[4].SqlDbType = SqlDbType.VarChar;
+            Parametros[4].SqlValue = email;
+
+            Parametros[5] = new SqlParameter();
+            Parametros[5].ParameterName = "@Integrado";
+            Parametros[5].SqlDbType = SqlDbType.Int;
+            Parametros[5].SqlValue = Integrado;
+
+            Parametros[6] = new SqlParameter();
+            Parametros[6].ParameterName = "@FechaRegistro";
+            Parametros[6].SqlDbType = SqlDbType.DateTime;
+            Parametros[6].SqlValue = FechaRegistro;
+
+        }
+        public Boolean mtd_registrar()
+        {
+            v_query = " INSERT INTO Contact (Id,Nombre,Apellido,Celular,email,Integrado,FechaRegistro)" +
+                      " VALUES (@Id,@Nombre,@Apellido,@Celular,@email,@Integrado,@FechaRegistro)";
+
+            mtd_asignaParametros();
+            v_ok = cls_datos.mtd_registrar(Parametros, v_query);
+ 
+            return v_ok;
+        }
+    }
+}
